fad43c4e34473bc82d5a343a3ca2f651aded9dce
JustineStrescher <justine.cioe@gmail.com>
2022-09-08 14:16:20 +0200

    product and navbar

--- src/Controller/PrestationController.php
+++ src/Controller/PrestationController.php
@@ -2,11 +2,18 @@
 
 namespace App\Controller;
 
+use App\Classe\Search;
 use App\Entity\Prestation;
+use App\Form\SearchType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+
+
+
 
 class PrestationController extends AbstractController
 {
@@ -20,16 +27,38 @@ class PrestationController extends AbstractController
 
     #[Route('/prestations', name: 'app_prestations')]
 
-    public function index()
+    public function index(Request $request): Response
     {
 
         $prestations = $this->entityManager->getRepository(Prestation::class)->findAll();
-        
 
-        return $this->render('prestation/index.html.twig',
-        [
-            'prestations' => $prestations
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            //dd($search);
+            $prestations= $this->entityManager->getRepository(Prestation::class)->findWithSearch($search);
+        }
+        
+        return $this->render('prestation/index.html.twig',[
+            'prestations' => $prestations,
+            'form' =>$form->createView()
         ]);
+    }
+    #[Route('/prestation/{slug}', name: 'app_prestation')]
+    public function show($slug): Response
+    {$prestation = $this->entityManager->getRepository(Prestation::class)->findOneBySlug($slug);
+        
+        if(!$prestation){
+            return $this->redirectToRoute('app_prestations');
+        }
+        return $this->render('prestation/show.html.twig',[
+            'prestation' => $prestation
+        ]);}
+   
         
     }
-}
+
+   
--- src/Repository/PrestationRepository.php
+++ src/Repository/PrestationRepository.php
@@ -2,17 +2,16 @@
 
 namespace App\Repository;
 
+use App\Classe\Search;
 use App\Entity\Prestation;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Prestation>
- *
- * @method Prestation|null find($id, $lockMode = null, $lockVersion = null)
- * @method Prestation|null findOneBy(array $criteria, array $orderBy = null)
- * @method Prestation[]    findAll()
- * @method Prestation[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Product|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Product|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Product[]    findAll()
+ * @method Product[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class PrestationRepository extends ServiceEntityRepository
 {
@@ -21,46 +20,60 @@ class PrestationRepository extends ServiceEntityRepository
         parent::__construct($registry, Prestation::class);
     }
 
-    public function add(Prestation $entity, bool $flush = false): void
+     /**
+     * Requète qui me permet de récuperer les produis en fonction
+     * de la recherche de l'utilisateur
+     * @return Prestation[]
+     */
+    public function findWithSearch(Search $search)
     {
-        $this->getEntityManager()->persist($entity);
+        $query = $this
+            ->createQueryBuilder('p')
+            ->select('c', 'p')
+            ->join('p.category', 'c');
 
-        if ($flush) {
-            $this->getEntityManager()->flush();
+        if (!empty($search->categories)){
+            $query = $query
+                ->andWhere('c.id IN (:categories)')
+                ->setParameter('categories', $search->categories);
         }
+
+        if (!empty($search->string)){
+            $query = $query
+            ->andWhere('p.name LIKE :string')
+            ->setParameter('string', "%{$search->string}%");
+        }
+    
+        return  $query->getQuery()->getResult();
     }
 
-    public function remove(Prestation $entity, bool $flush = false): void
+
+    // /**
+    //  * @return Product[] Returns an array of Product objects
+    //  */
+    /*
+    public function findByExampleField($value)
     {
-        $this->getEntityManager()->remove($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('p.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
     }
+    */
 
-//    /**
-//     * @return Prestation[] Returns an array of Prestation objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Prestation
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    /*
+    public function findOneBySomeField($value): ?Product
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
 }
